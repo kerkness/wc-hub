07374cbd64ff1053845344b78e43e965ec389573
kerkness <ryan@kerkness.ca>
2021-08-04 21:34:49 +0000

    tagged

--- woohub.php
+++ woohub.php
@@ -10,7 +10,7 @@
  * @wordpress-plugin
  * Plugin Name: Woohub
  * Description: WooCommerce & HubSpot Integration
- * Version:     0.1.0
+ * Version:     0.1.2
  * Author:      Ryan Mayberry (@kerkness)
  * Author URI:  https://kerkness.ca
  * Text Domain: woohub
